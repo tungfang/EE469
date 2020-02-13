a8870d0ffe2b5bd7b1cd9f6e0a37ff47b784b566
Charles Tung Fang <tungfang@uw.edu>
2020-02-13 00:22:45 -0800

    start the control path

---
+++ lab2/control.sv
@@ -0,0 +1,10 @@
+module control(instruction, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
+    input logic [5:0] instruction;
+    output logic RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite;
+    
+
+endmodule
+
+module control_testbench();
+
+endmodule
