fd3647d9df3992118a192dda4e4301254d27734b
Charles Tung Fang <tungfang@uw.edu>
2020-02-14 10:35:12 -0800

    update control

--- lab2/control.sv
+++ lab2/control.sv
@@ -2,7 +2,9 @@ module control(instruction, RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, 
     input logic [5:0] instruction;
     output logic RegDst, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite;
     
+    logic R_format, lw, sw, beq;
 
+    
 endmodule
 
 module control_testbench();
